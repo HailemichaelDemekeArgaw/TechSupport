d970b3787e194a4d0e1de5eb59adeb9cbdc845e4
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-01-30 01:09:34 -0800

    Added MainFormController with event handlers and data binding

--- TechSupport/Controller/MainFormController.cs
+++ TechSupport/Controller/MainFormController.cs
@@ -1,12 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TechSupport.View;
+using TechSupport.DAL;
 
 namespace TechSupport.Controller
 {
-    internal class MainFormController
+    /// <summary>
+    /// Implement MainFormController for MainFrom events and data binding.
+    /// </summary>
+    public static class MainFormController
     {
+        public static MainForm mainForm;
+        public static DataGridView dataGridView;
+
+        /// <summary>
+        /// Logout button event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void LogoutButton_Event(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            mainForm.Close();
+        }
+
+        /// <summary>
+        /// on load event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void OnLoad_Event(object sender, EventArgs e)
+        {
+            MainDataGridBinding();
+        }
+
+        /// <summary>
+        /// Method for binding data to datagrid
+        /// </summary>
+        public static void MainDataGridBinding()
+        {
+            IncidentService incidentService = new IncidentService();
+            var incidentsList = incidentService.GetIncidentList(0);
+            dataGridView.DataSource = incidentsList;
+        }
+
+        /// <summary>
+        /// Add incident event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void AddIncidentButton_Event(object sender, EventArgs e)
+        {
+            AddIncident addIncident = new AddIncident();
+            addIncident.ShowDialog();
+        }
+
+        /// <summary>
+        /// search incident event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void SearchIncidentButton_Event(object sender, EventArgs e)
+        {
+            SearchForm searchForm = new SearchForm();
+            searchForm.ShowDialog();
+        }
     }
 }
