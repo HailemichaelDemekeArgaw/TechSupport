7b39ded0ad4c71912b21e9a546132ad835f06dd7
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-01-30 01:19:58 -0800

    Implemented AddIncident form with event handlers and controller bindings

--- TechSupport/View/AddIncident.cs
+++ TechSupport/View/AddIncident.cs
@@ -1,20 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using TechSupport.Controller;
 
 namespace TechSupport.View
 {
+    /// <summary>
+    /// Class reprsenting the Add Incident Form.
+    /// </summary>
+    /// <seealso cref="System.Windows.Forms.Form" />
     public partial class AddIncident : Form
     {
         public AddIncident()
         {
             InitializeComponent();
+            titleTxt.KeyDown += AddIncidentController.TitleKeyDown_Event;
+            descTxt.KeyDown += AddIncidentController.DescKeyDown_Event;
+            custIdTxt.KeyDown += AddIncidentController.CustIdKeyDown_Event;
+            addBtn.Click += AddIncidentController.AddButton_Event;
+            cancelBtn.Click += AddIncidentController.CancelButton_Event;
+            TitleErrMsg.Hide();
+            DescErrMsg.Hide();
+            CustIdErrMsg.Hide();
+            AddIncidentController.title = titleTxt;
+            AddIncidentController.description = descTxt;
+            AddIncidentController.customerId = custIdTxt;
+            AddIncidentController.addIncident = this;
+            AddIncidentController.titelErrLbl = TitleErrMsg;
+            AddIncidentController.descErrLbl = DescErrMsg;
+            AddIncidentController.custIdErrLbl = CustIdErrMsg;
         }
     }
 }
