bbfe64e53e2bd25a898a3e974b8ada497c03261e
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-01-30 00:41:31 -0800

    Initialized Login form controls and events

--- TechSupport/View/Login.cs
+++ TechSupport/View/Login.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using TechSupport.Controller;
 
 namespace TechSupport.View
 {
+    /// <summary>
+    /// Class Representing the Log in Form
+    /// </summary>
     public partial class Login : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the Login class.
+        /// </summary>
         public Login()
         {
-            //InitializeComponent();
+            InitializeComponent();
+            userNameTxt.KeyDown += LoginController.KeyDown_Event;
+            passwordTxt.KeyDown += LoginController.KeyDown_Event;
+            submitBtn.Click += LoginController.LoginButton_Click;
+            errorLabel.Hide();
+            LoginController.errorLabel = errorLabel;
+            LoginController.loginText = userNameTxt;
+            LoginController.passwordText = passwordTxt;
+            LoginController.login = this;
         }
     }
 }
