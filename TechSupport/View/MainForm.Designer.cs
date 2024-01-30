a0feae6a2e1fe6e2d733b82fe088806b0a9f6b03
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-01-30 01:04:50 -0800

    Added UI components and event handlers in MainFrom

--- TechSupport/View/MainForm.Designer.cs
+++ TechSupport/View/MainForm.Designer.cs
@@ -1,16 +1,19 @@
-﻿namespace TechSupport.View
+﻿using TechSupport.Controller;
+
+namespace TechSupport.View
 {
+    /// <summary>
+    /// Represents the Main FORM UI features
+    /// </summary>
     partial class MainForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+       
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,28 +23,108 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Initializes the component.
         /// </summary>
         private void InitializeComponent()
         {
+            logoutLink = new LinkLabel();
+            userNameLbl = new Label();
+            incidentDatagrid = new DataGridView();
+            addBtn = new Button();
+            searchBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)incidentDatagrid).BeginInit();
             SuspendLayout();
+
             // 
-            // MainForm
+            // logoutLink
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            logoutLink.AutoSize = true;
+            logoutLink.Cursor = Cursors.Hand;
+            logoutLink.Location = new Point(574, 18);
+            logoutLink.Margin = new Padding(2, 0, 2, 0);
+            logoutLink.Name = "logoutLink";
+            logoutLink.Size = new Size(56, 20);
+            logoutLink.TabIndex = 4;
+            logoutLink.TabStop = true;
+            logoutLink.Text = "Logout";
+
+            // 
+            // userNameLbl
+            // 
+            userNameLbl.AutoSize = true;
+            userNameLbl.Location = new Point(473, 18);
+            userNameLbl.Margin = new Padding(2, 0, 2, 0);
+            userNameLbl.Name = "userNameLbl";
+            userNameLbl.Size = new Size(78, 20);
+            userNameLbl.TabIndex = 3;
+            userNameLbl.Text = "UserName";
+
+            // 
+            // incidentDatagrid
+            // 
+            incidentDatagrid.BackgroundColor = SystemColors.Window;
+            incidentDatagrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            incidentDatagrid.Location = new Point(105, 116);
+            incidentDatagrid.Margin = new Padding(2);
+            incidentDatagrid.Name = "incidentDatagrid";
+            incidentDatagrid.RowHeadersWidth = 62;
+            incidentDatagrid.Size = new Size(485, 335);
+            incidentDatagrid.TabIndex = 5;
+
+            // 
+            // addBtn
+            // 
+            addBtn.BackColor = Color.White;
+            addBtn.Location = new Point(63, 18);
+            addBtn.Margin = new Padding(2);
+            addBtn.Name = "addBtn";
+            addBtn.Size = new Size(121, 36);
+            addBtn.TabIndex = 0;
+            addBtn.Text = "Add Incident";
+            addBtn.UseVisualStyleBackColor = false;
+
+            // 
+            // searchBtn
+            // 
+            searchBtn.BackColor = SystemColors.ButtonHighlight;
+            searchBtn.Location = new Point(189, 18);
+            searchBtn.Margin = new Padding(2);
+            searchBtn.Name = "searchBtn";
+            searchBtn.Size = new Size(121, 36);
+            searchBtn.TabIndex = 1;
+            searchBtn.Text = "Search Incident";
+            searchBtn.UseVisualStyleBackColor = false;
+
+            // 
+            // MainFrom
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            MaximizeBox = false;
-            MinimizeBox = false;
-            Name = "MainForm";
-            Text = "MainFrom";
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(732, 553);
+            Controls.Add(searchBtn);
+            Controls.Add(addBtn);
+            Controls.Add(incidentDatagrid);
+            Controls.Add(userNameLbl);
+            Controls.Add(logoutLink);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(2);
+            Name = "MainFrom";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)incidentDatagrid).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
-        #endregion
+        private LinkLabel logoutLink;
+        private Label userNameLbl;
+        private DataGridView incidentDatagrid;
+        private DataGridViewTextBoxColumn Title;
+        private DataGridViewTextBoxColumn Description;
+        private DataGridViewTextBoxColumn customerID;
+        private Button addBtn;
+        private Button searchBtn;
     }
 }
