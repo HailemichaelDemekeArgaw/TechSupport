4de878ced7f9406e0cf8db633a765cbcbe515de1
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-01-30 00:57:52 -0800

    Added event handlers and bindings in the Main Form

--- TechSupport/View/MainForm.cs
+++ TechSupport/View/MainForm.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using TechSupport.Controller;
 
 namespace TechSupport.View
 {
+    /// <summary>
+    /// Declare the MainForm class.
+    /// </summary>
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the MainForm class.
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
+            logoutLink.Click += MainFormController.LogoutButton_Event;
+            addBtn.Click += MainFormController.AddIncidentButton_Event;
+            searchBtn.Click += MainFormController.SearchIncidentButton_Event;
+            MainFormController.dataGridView = incidentDatagrid;
+            MainFormController.mainForm = this;
+            MainFormController.MainDataGridBinding();
         }
     }
 }
