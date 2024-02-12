f33d8c9f7ba303a872dd73439e303ddccdaafdf5
Hailemichael Argaw <ha00071@my.westga.edu>
2024-02-12 12:40:43 -0500

    Configured a data base liberary to the project and crated DBAccess class with a GetSqlConnection method

---
+++ TechSupport/DAL/DBAccess .cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace TechSupport.DAL
+{
+    public class DBAccess
+    {
+        /// <summary>
+        /// Method to get Database connection
+        /// </summary>
+        /// <returns>return Database SQL connection</returns>
+        public static SqlConnection GetSqlConnection()
+        {
+            var conStr = "Data Source=.\\SQLEXPRESS; Initial Catalog=TechSupport; Integrated Security=true";
+            return new SqlConnection(conStr);
+        }
+
+        /// <summary>
+        /// Method to get list of data from database based on sql statment.
+        /// </summary>
+        /// <param name="sql">SQL Statement</param>
+        /// <returns>Return Data table</returns>
+
+        public static DataTable? ReturnDataTable(string sql)
+        {
+            try
+            {
+                SqlConnection connection = GetSqlConnection();
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                connection.Close();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                connection.Close();
+                command.Dispose();
+                return dataTable;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
+
