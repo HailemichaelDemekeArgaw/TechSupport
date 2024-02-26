9b5d309f2dc50e879921a66102bee3af39260202
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-02-26 16:42:38 -0500

    update cllass with  properties

--- TechSupport/Model/Incidents.cs
+++ TechSupport/Model/Incidents.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TechSupport.Model
 {
-    internal class Incidents
+    public class Incidents
     {
+        public int IncidentID { get; set; }
+        public int CustomerId { get; set; }
+        public string ProductCode { get; set; }
+        public int TechId { get; set; }
+        public DateTime DateOpened { get; set; }
+        public DateTime DateClosed { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+
+
     }
 }
