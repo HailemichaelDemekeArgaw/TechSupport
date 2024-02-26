df46caa80e99f9ee0acbdc606011afeab8b1dba8
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-02-26 16:48:03 -0500

    Updated Products class with properties for ProductCode (string, Key), Name (string), Version (decimal), and ReleaseDate (DateTime)

--- TechSupport/Model/Products.cs
+++ TechSupport/Model/Products.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TechSupport.Model
 {
-    internal class Products
+    public class Products
     {
+        [Key]
+        public string ProductCode { get; set; }
+        public string Name { get; set; }
+        public decimal Version { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+
+
     }
 }
