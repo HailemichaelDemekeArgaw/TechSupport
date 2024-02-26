506dd45783664409431edcc2c58abfc422257cdf
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-02-26 15:12:23 -0500

    Added CustomersController with basic constructor and GetCustomers method

--- TechSupport/Controller/CustomersController.cs
+++ TechSupport/Controller/CustomersController.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechSupport.DAL;
+using TechSupport.Model;
 
 namespace TechSupport.Controller
 {
-    internal class CustomersController
+    public class CustomersController
     {
+        private readonly CustomerDAL _customerDAL;
+        /// <summary>
+        /// CustomersController constructor.
+        /// 
+        /// </summary>
+
+        public CustomersController()
+        {
+            this._customerDAL = new CustomerDAL();
+        }
+
+
+        /// <summary>
+        /// Get a list of customer.
+        /// </summary>
+        /// <returns>return list of customer</returns>
+        public List<Customers> GetCustomers()
+        {
+            return this._customerDAL.GetCustomers();
+        }
     }
 }
