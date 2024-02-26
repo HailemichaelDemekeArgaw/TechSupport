98a2ea03673ee4f82e11020334797010a216aa1f
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-02-26 16:34:37 -0500

    Implemented TechniciansDAL with GetTechnicians method for retrieving technician data from the database

--- TechSupport/DAL/TechniciansDAL.cs
+++ TechSupport/DAL/TechniciansDAL.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechSupport.Model;
 
 namespace TechSupport.DAL
 {
-    internal class TechniciansDAL
+    public class TechniciansDAL
     {
+        public List<Technicians> GetTechnicians()
+        {
+            List<Technicians> technicianList = new List<Technicians>();
+            string sqlStatement = "select * from Technicians";
+            using (SqlConnection connection = DBAccess.GetSqlConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(sqlStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Technicians technician = new Technicians();
+                            technician.TechID = (int)reader["TechID"];
+                            technician.Name = (string)reader["Name"];
+                            technician.Email = (string)reader["email"];
+                            technician.Phone = (string)reader["Phone"];
+                            technicianList.Add(technician);
+                        }
+                    }
+                }
+            }
+            return technicianList;
+        }
+
     }
 }
