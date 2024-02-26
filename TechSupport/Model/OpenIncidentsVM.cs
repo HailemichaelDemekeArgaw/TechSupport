62cc64530f22e05ab238cbdf247769d7bce1653c
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-02-26 16:44:12 -0500

    Updated OpenIncidentsVM class with properties for ProductCode (string), DatedOpened (string), Customer (string), Title (string?), and Technician (string?)

--- TechSupport/Model/OpenIncidentsVM.cs
+++ TechSupport/Model/OpenIncidentsVM.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace TechSupport.Model
 {
-    internal class OpenIncidentsVM
+    public class OpenIncidentsVM
     {
+        public string ProductCode { get; set; }
+        public string DatedOpened { get; set; }
+        public string Customer { get; set; }
+        public string? Title { get; set; }
+        public string? Technician { get; set; }
     }
 }
