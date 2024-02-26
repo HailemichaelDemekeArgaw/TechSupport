812a2100176ded015f783f8999eae3ac716c983d
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-02-26 15:41:05 -0500

    Implemented CustomerDAL with GetCustomers method for retrieving customer data from the database.

--- TechSupport/DAL/CustomerDAL.cs
+++ TechSupport/DAL/CustomerDAL.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechSupport.Model;
 
 namespace TechSupport.DAL
 {
-    internal class CustomerDAL
+    /// <summary>
+    /// This the DAL class that deals with Customer.
+    /// </summary>
+    public class CustomerDAL
     {
+        public List<Customers> GetCustomers()
+        {
+            List<Customers> customerList = new List<Customers>();
+            string sqlStatement = "select * from Customers";
+            using (SqlConnection connection = DBAccess.GetSqlConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(sqlStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Customers customer = new Customers();
+
+                            customer.CustomerId = (int)reader["CustomerID"];
+                            customer.Name = (string)reader["Name"];
+                            customer.Address = (string)reader["address"];
+                            customer.City = (string)reader["City"];
+                            customer.ZipCode = (string)reader["ZipCode"];
+                            customer.Phone = (string)reader["Phone"];
+                            customer.Email = (string)reader["email"];
+                            customerList.Add(customer);
+
+
+                        }
+                    }
+                }
+            }
+            return customerList;
+        }
     }
 }
+
