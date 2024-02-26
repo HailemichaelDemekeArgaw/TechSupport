f0af1d4e5e9b4e4daf58b76591f2c06b44e8bf20
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-02-26 16:37:48 -0500

    Updated class with properties for CustomerId, Name, Address, City, State, ZipCode, Phone, and Email

--- TechSupport/Model/Customers.cs
+++ TechSupport/Model/Customers.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace TechSupport.Model
 {
-    internal class Customers
+    public class Customers
     {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
     }
 }
