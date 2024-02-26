c136d4ec1e4f9eefc7c81411e5eafa8b2a02a357
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-02-26 15:33:24 -0500

    Implemented ProductsController with constructor and GetProducts method

--- TechSupport/Controller/ProductsController.cs
+++ TechSupport/Controller/ProductsController.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechSupport.DAL;
+using TechSupport.Model;
+
 
 namespace TechSupport.Controller
 {
-    internal class ProductsController
+    public class ProductsController
     {
+        private readonly ProductDAL _productDAL;
+        public ProductsController()
+        {
+            this._productDAL = new ProductDAL();
+        }
+
+        public List<Products> GetProducts()
+        {
+            return this._productDAL.GetProducts();
+        }
+
     }
 }
