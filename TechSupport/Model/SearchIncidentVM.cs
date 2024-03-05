c4dded11703eec47715d163432af4f7a27dd7dd9
HailemichaelDemekeArgaw <ha00071@my.westga.edu>
2024-03-04 21:10:26 -0500

    Added a search Incidnet model class

---
+++ TechSupport/Model/SearchIncidentVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Model
+{
+    public class SearchIncidentVM
+    {
+        public int IncidentId { get; set; }
+        public int CustomerId { get; set; }
+        public string ProductCode { get; set; }
+        public int? TechId { get; set; }
+        public DateTime DatedOpened { get; set; }
+        public DateTime? DateClosed { get; set; }
+        public string? Title { get; set; }
+        public string? Technician { get; set; }
+        public string? Description { get; set; }
+        public string Customer { get; set; }
+    }
+}
